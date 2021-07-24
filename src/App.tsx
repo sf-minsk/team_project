d3c33719f7dcad471670de75559121e7c3e4b68d
sf-minsk <sf-minsk@outlook.com>
2021-07-24 17:54:55 +0300

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import {NavLink, Redirect, Route, Switch} from 'react-router-dom';
 import './App.css';
+import {Cards} from "./components/Cards/Cards";
+import {Registration} from "./components/auth/Registration/Registration";
+import {Login} from "./components/auth/Login/Login";
+import {ChangePassword} from "./components/auth/ChangePassword/ChangePassword";
 
 function App() {
     return (
         <>
             <div>
-                <button>HOME</button>
-                <button>Register</button>
-                <button>Login</button>
-                <button>Change Pasword</button>
+                <button><NavLink to='/'>Home</NavLink></button>
+                <button><NavLink to='/registration'>Register</NavLink></button>
+                <button><NavLink to='/login'>Login</NavLink></button>
+                <button><NavLink to='/changepassword'>Change Pasword</NavLink></button>
             </div>
-            <Route path='/profile/:userId?' render={() =>
-                <ProfileContainer/>}
-            />
+            <div>
+                <Switch>
+                    <Route exact path={'/'} render={() => <Cards/>}/>
+                    <Route path={'/registration'} render={() => <Registration/>}/>
+                    <Route path={'/login'} render={() => <Login/>}/>
+                    <Route path={'/changepassword'} render={() => <ChangePassword/>}/>
+                    <Redirect from={'*'} to={'/'}/>
+                </Switch>
+            </div>
+
         </>
     );
 }
