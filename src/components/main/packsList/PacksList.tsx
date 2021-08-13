f5f516c5f722d8eacd540e744458d0fa22f96aff
sf-minsk <sf-minsk@outlook.com>
2021-08-13 18:20:57 +0300

    final

--- src/components/main/packsList/PacksList.tsx
+++ src/components/main/packsList/PacksList.tsx
@@ -57,7 +57,7 @@ export const PacksList: React.FC = React.memo(() => {
         setSearchText(value)
         const newIntervalID = setTimeout(() => {
             dispatch(setCardPacksTC({packName: value, page: 1}))
-        }, 700)
+        }, 800)
         setIntervalID(newIntervalID)
     }
 
